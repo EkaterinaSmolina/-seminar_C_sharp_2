901ba37ae85c09a6d8cf84fc1d63adbbcc310ece
Smolina <katerina_kat2101@mail.ru>
2022-08-13 18:49:57 +1000

    дз2

--- Program.cs
+++ Program.cs
@@ -1,37 +1,47 @@
-﻿Console.WriteLine("введите трехзначное число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-int num1 = number1/10;
-int num2= number1%100;
-int num3= num2/10;
-Console.WriteLine(num3);
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+//Console.WriteLine("введите трехзначное число: ");
+//int number1 = Convert.ToInt32(Console.ReadLine());
+//int num1 = number1/10;
+//int num2= number1%100;
+//int num3= num2/10;
+//Console.WriteLine(num3);
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
 
 
 
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
 
 
+// Console.WriteLine("введите число от 1 до 7: ");
+// int day = Convert.ToInt32(Console.ReadLine());
 
-
-
-
-
-
-
-
-
-
-
-
-// int number1 = new Random().Next(10,1000);
-// Console.WriteLine(number1);
-// int num1 = number1/100;
-// Console.WriteLine(num1);
-// int num2 = number1%10;
-// Console.WriteLine(num2);
-// if (num1>num2)
+// if (day>=1 && day<=5)
 // {
-//     Console.WriteLine(num1);    
+//   Console.WriteLine("нет");  
 // }
-// else 
+// else if (day==6 || day==7)
 // {
-//     Console.WriteLine(num2);
+//    Console.WriteLine("да");
 // }
+//  else 
+//    Console.WriteLine("не верное число");
+
+
+
+
+
