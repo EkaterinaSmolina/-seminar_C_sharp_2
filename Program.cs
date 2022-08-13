060516f7892f19bc7a9223c0c3fe3631c549ea43
Smolina <katerina_kat2101@mail.ru>
2022-08-13 15:55:11 +1000

    задача1

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("введите трехзначное число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int num1 = number1/10;
+int num2= number1%100;
+int num3= num2/10;
+Console.WriteLine(num3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int number1 = new Random().Next(10,1000);
+// Console.WriteLine(number1);
+// int num1 = number1/100;
+// Console.WriteLine(num1);
+// int num2 = number1%10;
+// Console.WriteLine(num2);
+// if (num1>num2)
+// {
+//     Console.WriteLine(num1);    
+// }
+// else 
+// {
+//     Console.WriteLine(num2);
+// }
